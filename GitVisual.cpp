6df7a1d978eb54c48a9d38cce277e3c7edef5750
anxoroca <anxoroca@gmail.com>
2022-03-12 22:00:51 +0100

    Versión 2.0 Moreno

---
+++ GitVisual.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    cout << "Cambios Stage Versión 2.0" << endl;
+    printf ("Characters: %c %c \n", 'a', 65);
+	return 0;
+}
